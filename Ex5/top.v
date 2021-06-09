c582e402fbadf9819fcb35e8ac51649206323aa4
Zipei Liu <lzp113@outlook.com>
2021-06-09 15:14:10 +0000

    FSM inplementation of Ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name:Zipei Liu
+// Date: 09 June 2021
 //
 //  Description: In this exercise, you need to an air conditioning control system
 //  According to the state diagram provided in the exercise.
@@ -12,4 +12,71 @@
 //  outputs:
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns/100ps
 
+module aircon(input clk, input [4:0] temperature, output reg heating,cooling);
+	reg [1:0] state;
+	parameter S0 = 0,S1 = 1,S2 = 2;
+
+	always @(state) begin
+		case (state)
+			S0: 
+			begin
+				heating = 1;
+				cooling = 0;
+			end
+			S1:
+			begin
+				heating = 0;
+				cooling = 1;
+			end
+			S2:
+			begin
+				heating = 0;
+				cooling = 0;
+			end
+			default: 
+			begin
+				heating = 0;
+				cooling = 0;
+			end
+		endcase
+	end
+	
+	always @(posedge clk) begin
+		case(state)
+			S0:
+			begin
+				if(temperature>=5'd20) begin
+					state <= S2;
+				end
+				else begin
+					state <= S0;
+				end
+			end
+			S1:
+			begin
+				if(temperature<=5'd20) begin
+					state <= S2;
+				end
+				else begin
+					state <= S1;
+				end
+			end
+			S2:
+			begin
+				if(temperature<=5'd18) begin
+					state <= S0;
+				end
+				else begin 
+					if(temperature>=5'd22) begin
+						state <= S1;
+					end
+					else begin
+						state <= S2;
+					end
+				end
+			end
+		endcase
+	end
+endmodule
