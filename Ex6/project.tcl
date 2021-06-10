b8d938a55a954d67c8923fe06b52d02286ed8fd3
Zipei Liu <lzp113@outlook.com>
2021-06-10 15:44:20 +0000

    halfway done on Ex6

--- Ex6/project.tcl
+++ Ex6/project.tcl
@@ -21,6 +21,11 @@ create_fileset -constrset -quiet constraints
 #set_property is_enabled false [get_files ${project_constraints}]
 
 #Todo: add verilog modules here
+
+create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name mybram
+set_property -dict [list CONFIG.Component_Name {mybram} CONFIG.Memory_Type {Single_Port_RAM} CONFIG.Use_Byte_Write_Enable {false} CONFIG.Write_Width_A {24} CONFIG.Write_Depth_A {6} CONFIG.Read_Width_A {24} CONFIG.Operating_Mode_A {WRITE_FIRST} CONFIG.Write_Width_B {24} CONFIG.Read_Width_B {24} CONFIG.Enable_B {Always_Enabled} CONFIG.Register_PortA_Output_of_Memory_Primitives {true} CONFIG.Register_PortB_Output_of_Memory_Primitives {false} CONFIG.Use_RSTA_Pin {false} CONFIG.Port_B_Clock {0} CONFIG.Port_B_Write_Rate {0} CONFIG.Port_B_Enable_Rate {0} CONFIG.EN_SAFETY_CKT {false}] [get_ips mybram]
+set_property -dict [list CONFIG.Write_Depth_A {8} CONFIG.Load_Init_File {true} CONFIG.Coe_File {/home/centos/CWM-ECAD/Ex6/mem.coe}] [get_ips mybram]
+
 read_verilog "top.v"
 read_verilog "top_tb.v"
 
--- Ex6/top.v
+++ Ex6/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name: Zipei Liu
+// Date: 10 June 2021
 //
 //
 //  Description: In this exercise, you need to design a memory with 8 entries, 
@@ -14,4 +14,19 @@
 //           rgb [23:0]
 //
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns/100ps
 
+module module_name(input clk, enable,input [2:0]  color,output [23:0] rgb);
+
+	//instantiate bram ip core
+	mybram converter(
+	  .clka(clk),    // input wire clka
+	  .ena(enable),      // input wire ena
+	  .wea(1'b0),      // input wire [0 : 0] wea
+	  .addra(color),  // input wire [2 : 0] addra
+	  .dina(24'b0),    // input wire [23 : 0] dina
+	  .douta(rgb)  // output wire [23 : 0] douta
+	);
+
+	
+endmodule
