f62bb8ecfd241f6ab35ed6746e34b05608526cf1
Zipei Liu <lzp113@outlook.com>
2021-06-11 12:44:14 +0000

    with minor error

--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -14,9 +14,6 @@ module top_tb();
 	reg clk, enable, err;
 	reg [2:0] color;
 	wire [23:0] rgb;
-	reg [191:0] mem = {24'h000000, 24'h0000FF, 24'h00FF00, 24'h00FFFF, 24'hFF0000, 24'hFF00FF, 24'hFFFF00, 24'hFFFFFF};
-	reg [8:0] index_start = 0;
-	reg [8:0] index_end = 8'd23;
 
 	initial begin
 		clk = 1'b1;
@@ -27,22 +24,21 @@ module top_tb();
 	initial begin
 		err=0;
 		enable=0;
-		color=3'b001;
-		#10
+		color=3'b011;
+		#20
 		if(rgb) begin
 			$display("***TEST FAILED! enable not working.***");
 			err=1;
 		end
 
-		enable=1;		
-		for(color=3'b000;color<=3'b111;color=color+3'b001) begin
-			#10
-			if(rgb!=mem[index_start:index_end]) begin
+		enable=1;
+		forever begin	
+			#20
+			if((0-rgb[7:0])!=color[0]|(0-rgb[15:8])!=color[1]|(0-rgb[23:16])!=color[2]) begin
 				$display("***TEST FAILED! converter not working.***");
 				err=1;
 			end
-			index_start = index_start+24;
-			index_end = index_end+24;
+			color = color+1;
 		end
 	end
 
